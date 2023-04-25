eee73fe4ea2361278fe686be232c18dd8bd1d9a3
Santiago Lobo <lobo09.slu@gmail.com>
2023-04-24 22:47:32 -0600

    Generación de la imagen para la Landing Page

--- visit-view/src/components/landing.jsx
+++ visit-view/src/components/landing.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import background from "../assets/background.jpg";
+import WhiteLogo from "../assets/whiteLogo.png";
 class Landing extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="">
-          <div className="h-[94.2%] w-full absolute bg-emerald-800 z-50 opacity-70">
-            <div>
-              <p>Visit View</p>
-            </div>
+        <div className="overflow-y-hidden">
+          <div className="h-[94.2%] w-full absolute bg-emerald-800 z-50 opacity-70"></div>
+          <div className="absolute opacity-100 z-50 m-auto ">
+            <img
+              src={WhiteLogo}
+              alt="White Logo"
+              className=" h-20  opacity-100"
+            />
           </div>
           <div className="h-[94.2%] w-full absolute">
             <img className="h-[100%] w-full" src={background} alt="" />
